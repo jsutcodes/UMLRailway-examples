673e31ed77bf623946bd97499d679e4ba3d8d44d
Jordan Sutton <jorsutton92@gmail.com>
2019-06-21 16:58:31 -0600

    Update in1.java

--- interface/in1.java
+++ interface/in1.java
@@ -11,3 +11,21 @@ interface in1
     // public and abstract  
     void display(); 
 } 
+
+
+/**
+* UML Diagram Expected Output:
+
+-------------------------
+*    <<interface>>      *
+*         in1           *
+-------------------------
+*   + A : int           *
+*   + display(): void   *
+-------------------------
+
+Note: Capital A bc of final keyword
+and <<interface>> bc of keyword interface
+
+
+**/
