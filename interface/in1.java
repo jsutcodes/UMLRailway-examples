2769de2d5e44629a9596845b15418630bcb130bb
Jordan Sutton <jorsutton92@gmail.com>
2019-06-21 16:53:33 -0600

    Create in1.java

---
+++ interface/in1.java
@@ -0,0 +1,13 @@
+// Java program to demonstrate working of  
+// interface. 
+import java.io.*; 
+  
+// A simple interface 
+interface in1 
+{ 
+    // public, static and final 
+    final int a = 10; 
+  
+    // public and abstract  
+    void display(); 
+} 
